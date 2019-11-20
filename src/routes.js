693d513f7ac0e61c691f640ef604b10ae392eae6
davim5 <davim53@hotmail.com>
2019-11-20 20:33:32 -0300

    Appointments, schedules and pages

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,9 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 // Middlewares
 import authMiddleware from './app/middlewares/auth';
@@ -19,6 +22,15 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/appointments', AppointmentController.index);
+
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
