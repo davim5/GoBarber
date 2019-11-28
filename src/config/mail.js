9c55ae282036f7aa9129265e0127ac6004a4738e
davim5 <davim53@hotmail.com>
2019-11-27 22:19:44 -0300

    tentando template email

---
+++ src/config/mail.js
@@ -0,0 +1,21 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '9013dc80f78591',
+    pass: 'cb3c58ffe67fee',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/*
+Amazon SES
+Mailgun
+Sparkpost
+Gmail
+
+Mailtrap (DEV)
+*/
